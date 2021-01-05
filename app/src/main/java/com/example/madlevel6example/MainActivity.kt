00c26c920a6d4f2bb1dc2b52888da56fa72c5348
Pieter <pieter.miedema@hva.nl>
2021-01-05 10:23:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/madlevel6example/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.madlevel6example
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel: TriviaViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            viewModel.getTriviaNumber()
+        }
+    }
+}
